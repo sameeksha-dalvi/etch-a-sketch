46442c66bbdb3e9144b56dfc837879f07f827827
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-18 03:22:45 +0530

    Modify grid creation logic to dynamic creation.
    Grid creation logic was hardcoded to size 16 so created a function createGrid to dynamically create a new grid of given size.

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
 
 const container = document.querySelector(".container");
 
+createGrid(16);
 
-for(let i = 1 ; i <= 16; i++){
-    const row = document.createElement("div");
-    row.setAttribute("class","row");
-    container.appendChild(row);
-    for( let j = 1; j <= 16 ; j++){
-        const  col = document.createElement("div");
-        col.setAttribute("class","col");
-        row.appendChild(col);
+function createGrid(size) {
+
+    if (container.hasChildNodes()) {
+        container.textContent = "";
     }
-    
+
+    for (let i = 1; i <= size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        container.appendChild(row);
+        for (let j = 1; j <= size; j++) {
+            const col = document.createElement("div");
+            col.setAttribute("class", "col");
+            row.appendChild(col);
+        }
+
+    }
+
 }
 
-const square = document.querySelector(".container");
 
-square.addEventListener("mouseover",(e) =>{
+
+container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "gray";
 });
 
 
 const clearBtn = document.querySelector("#clear-grid");
 
-clearBtn.addEventListener("click", (e) =>{
-    console.log(e.target);
-    const  colNodeList = document.querySelectorAll(".col");
+clearBtn.addEventListener("click", (e) => {
+    const colNodeList = document.querySelectorAll(".col");
 
-    for(let i = 0 ; i < colNodeList.length; i ++ ){
+    for (let i = 0; i < colNodeList.length; i++) {
         colNodeList[i].style.backgroundColor = "white";
     }
-    
+
 });
 
 
 const changeGridSize = document.querySelector("#change-grid-size");
 
-changeGridSize.addEventListener("click", (e) =>{
+changeGridSize.addEventListener("click", (e) => {
     let gridSize = parseInt(prompt("Please enter the new grid size :"));
 
-    if(isNaN(gridSize)){
+    if (isNaN(gridSize)) {
         alert("Please enter valid number.");
         return false;
     }
 
-    if(gridSize > 100){
+    if (gridSize > 100) {
         alert("Maximum grid size limit is 100");
+        return false;
     }
-    console.log("gridSize "+gridSize);
-    console.log("gridSize "+typeof gridSize);
+    createGrid(gridSize);
 
 });
 
